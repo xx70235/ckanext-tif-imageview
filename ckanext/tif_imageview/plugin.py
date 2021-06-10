eb1b32b234a2b216f20f533445f0c8ce6e1d8f2f
pooya.oa <pooya.oladazimi@tib.eu>
2021-06-10 17:28:50 +0200

    initial

---
+++ ckanext/tif_imageview/plugin.py
@@ -0,0 +1,77 @@
+from os import read
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+from six import text_type
+from flask import Blueprint, request
+import ckan.lib.helpers as h
+from PIL import Image
+import io
+import ckan.lib.uploader as uploader
+import base64
+
+ignore_empty = plugins.toolkit.get_validator('ignore_empty')
+
+
+def convert(): 
+    
+    resource_id = request.form.get('resource_id')    
+    rsc = toolkit.get_action('resource_show')({}, {'id': resource_id})
+    upload = uploader.get_resource_uploader(rsc)
+    filepath = upload.get_path(rsc['id'])
+
+    file = open(filepath, "rb").read()
+    img = Image.open(io.BytesIO(file))    
+    output = io.BytesIO()
+    img.convert('RGB').save(output, 'JPEG')    
+    output.seek(0)        
+    return base64.b64encode(output.getvalue()).decode()
+
+
+class TifImageviewPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IResourceView, inherit=True)
+    plugins.implements(plugins.IBlueprint)
+
+
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'theme/templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'tif_imageview')
+        
+        
+    def info(self):
+        return {'name': 'tifviewer',
+            'title': plugins.toolkit._('TIF Viewer'),
+            'schema': {'tif_url': [ignore_empty, text_type]},
+            'iframed': False,
+            'icon': 'link',
+            'always_available': True,
+            'default_title': plugins.toolkit._('TIF Viewer'),
+        }
+    
+    def can_view(self, data_dict):
+        resource = data_dict['resource']
+        return (resource.get('format', '').lower() in ['tif', 'tiff' ] or
+                resource['url'].split('.')[-1] in ['tif'])
+
+    def view_template(self, context, data_dict):
+        return 'tif_view.html'
+
+    def form_template(self, context, data_dict):
+        return 'tif_form.html'
+
+    def get_blueprint(self):
+
+        blueprint = Blueprint(self.name, self.__module__)
+        blueprint.template_folder = u'templates'
+        blueprint.add_url_rule(
+            u'/tif_view/convert',
+            u'convert',
+            convert,
+            methods=['POST']
+            )
+        
+        return blueprint
